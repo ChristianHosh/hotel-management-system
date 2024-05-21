277429a79c601a5c473e35f4ae5c6bd4d1ced0ea
Christian Hosh <104357056+ChristianHosh@users.noreply.github.com>
2024-05-21 19:29:38 +0300

    admin user creator

---
+++ src/main/java/com/chris/hotelmanagementsystem/user/UserCreator.java
@@ -0,0 +1,29 @@
+package com.chris.hotelmanagementsystem.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserCreator {
+
+  private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void onApplicationEvent() {
+    var optional = repository.findByUsername("admin");
+
+    if (optional.isEmpty()) {
+      User user = new User();
+      user.setUsername("admin");
+      user.setPassword(passwordEncoder.encode("a12345"));
+      user.setRole(User.Role.ADMIN);
+      repository.save(user);
+    }
+  }
+
+}
